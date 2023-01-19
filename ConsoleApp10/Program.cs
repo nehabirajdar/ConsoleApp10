2587961d244f640e5e2963a2b40c5e854b7bc758
nehabirajdar <nsb9823@gmail.com>
2023-01-19 14:33:00 +0530

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 6, i, j, m = 1; 
+            Console.WriteLine(" Enter the number to define the columns: \n");
+
+            for (i = 1; i <= num; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine("");
+            }
+            for (i = num - 1; i >= 1; i--)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
+   
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows=8, k;
+            
+            Console.WriteLine("");
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = i; j < rows; j++)
+                {
+                    Console.WriteLine("");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*"); // print the Star  
+                }
+                Console.WriteLine("");
+            }
+            for (i = 1; i <= rows; ++i)
+            {
+                for (j = 2; j <= i; j++)
+                {
+                    Console.WriteLine("");
+                }
+                for (k = i; k <= rows; k++)
+                {
+                    Console.Write("*"); // print the Star  
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
